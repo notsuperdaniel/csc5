ade25a198dd78b48fd93070532cc547bc6afe772
notsuperdaniel <46760296+notsuperdaniel@users.noreply.github.com>
2019-02-10 18:14:49 -0800

    Add files via upload

---
+++ Projects/Final_PseudoCode/main.cpp
@@ -0,0 +1,377 @@
+/* 
+ * File:   main.cpp
+ * Author: Daniel Garcia
+ * Created on February 7, 2019, 10:04 PM
+ * Purpose:  BlackJack V8 Final Pseudo Code
+ */
+
+//System Libraries 
+//Input Output Library
+//Random Number Generator
+//Time to set the seed
+//File Stream Library
+//Math Library
+//Formatting Library
+//Namespace std of system libraries
+
+//User Libraries Here
+//Global Constants Only, No Global Variables
+//Like PI, e, Gravity, or conversions
+//Function Prototypes Here
+    //Menu
+    //Check money
+    //Add Money
+    //end program
+    //bet money
+    //random number
+    //Users Cards functions [1-6]
+    //Dealers Cards Functions [1-6]
+    //Ace functions [1-6]
+    //Bust
+    //Result
+    //Program Execution Begins Here
+    //Main
+    //Set The Random Number Seed
+    //Declare all Variables Here
+    //The players cards as numbers
+    //The dealers cards as numbers
+    //Card Array
+    //Money Bet
+    //Dealers Sum, Players Sum
+    //Players Ace, Dealers Ace,Players Choice to start,Players Choice to continue
+    //Players cards as string
+    //Dealers Cards as string
+    //If player decides to stay,Dealers turn 
+    //Input File
+    //Output file
+     
+    //Start Screen
+    //If User wants to play again, restarts here
+    //User enters (P) to start the game
+    //User Enters (M) to add money
+    //User Enters (S) to check money
+    //User Enters (E) to exit
+    //User Validation, Ask again until answer is (P),(M),(S),or(E)
+    //Initialize Sum and Dealers Sum to zero
+   
+    //Call Random Number function
+    //Call random Number Generator for Dealers Cards
+    //Value for User card 1 function
+    //Value for User Card 2 Function
+    //Value for Dealer Card 1 function
+    //Display the face of the 3 cards to the user
+    
+    
+   //Ask User if they want to hit
+   //Asks until user enters a (y) or (n)
+      //If user inputs Y, start of 3rd card
+          //Value for user card 3 function
+          //Display the user's 3 cards
+          //Display the dealers card
+    
+      //If User inputs (N)
+      //Display they decided to stay
+              //If card 1 is an Ace function
+                      //User chooses the value to 1 or 11
+                      //User Validation       
+             //If Card 2 is an Ace Function
+                     //User chooses value to equal 1 or 11
+                     //User Validation
+    
+             //Math for sum of the 2 cards
+             //Shows Card 1 Value
+             //Shows Card 2 Value
+             //DIsplays Sum 
+             //Shows Dealers Card
+             //Doesn't ask to hit or stay again
+             //Goes to Dealers turn
+    
+    
+    //If cont=true
+    //Ask User if they want to hit
+   //Asks until user enters a (y) or (n)
+      //If user inputs Y, start of 4th card
+          //Value for user card 4 function
+          //Display the user's 4 cards
+          //Display the dealers card
+    
+      //If User inputs (N)
+      //Display they decided to stay
+              //If card 1 is an Ace function
+                      //User chooses the value to 1 or 11
+                      //User Validation       
+             //If Card 2 is an Ace function
+                     //User chooses value to equal 1 or 11
+                     //User Validation
+             //If Card 3 is an Ace function
+                     //User chooses value to equal 1 or 11
+                     //User Validation
+    
+             //Math for sum of the 3 cards
+             //Shows Card 1 Value
+             //Shows Card 2 Value
+             //Shows card 3 value
+             //Dssplays Sum 
+             //Shows Dealers Card
+             //ask to hit or stay again=false
+             //Goes to Dealers turn=true
+    
+    
+   
+      //If cont=true
+    //Ask User if they want to hit
+   //Asks until user enters a (y) or (n)
+      //If user inputs Y, start of 5th card
+          //User Card 5 function
+          //Display the user's 5 cards
+          //Display the dealers card
+    
+      //If User inputs (N)
+      //Display they decided to stay
+              //If card 1 is an Ace function
+                      //User chooses the value to 1 or 11
+                      //User Validation       
+             //If Card 2 is an Ace function
+                     //User chooses value to equal 1 or 11
+                     //User Validation
+             //If Card 3 is an Ace function
+                     //User chooses value to equal 1 or 11
+                     //User Validation
+             //If Card 4 is an Ace function
+                     //User chooses value to equal 1 or 11
+                     //User Validation
+    
+             //Math for sum of the 4 cards
+             //Shows Card 1 Value
+             //Shows Card 2 Value
+             //Shows card 3 value
+             //Show card 4 Value
+             //Displays Sum 
+             //Shows Dealers Card
+             //ask to hit or stay again=false
+             //Goes to Dealers turn=true
+    
+    
+    
+          //If cont=true
+    //Ask User if they want to hit
+   //Asks until user enters a (y) or (n)
+      //If user inputs Y, start of 6th card
+          //User Card 6 function
+          //Display the user's 6 cards
+          //Display the dealers card
+    
+      //If User inputs (N)
+      //Display they decided to stay
+              //If card 1 is an Ace function
+                      //User chooses the value to 1 or 11
+                      //User Validation       
+             //If Card 2 is an Ace function
+                     //User chooses value to equal 1 or 11
+                     //User Validation
+             //If Card 3 is an Ace function
+                     //User chooses value to equal 1 or 11
+                     //User Validation
+             //If Card 4 is an Ace function
+                     //User chooses value to equal 1 or 11
+                     //User Validation
+             //If Card 5 is an Ace function
+                     //User chooses value to equal 1 or 11
+                     //User Validation
+    
+             //Math for sum of the 5 cards
+             //Shows Card 1 Value
+             //Shows Card 2 Value
+             //Shows card 3 value
+             //Show card 4 Value
+             //Show card 5 Value
+             //Dssplays Sum 
+             //Shows Dealers Card
+             //ask to hit or stay again=false
+             //Goes to Dealers turn=true
+   
+    // if cont=true
+    //Tells User they reached the card limit
+        //Press y to continue
+         //User Validation
+         //if (Y) the user stays
+      
+          //If card 1 is an Ace
+                      //User chooses the value to 1 or 11
+                      //User Validation       
+             //If Card 2 is an Ace
+                     //User chooses value to equal 1 or 11
+                     //User Validation
+             //If Card 3 is an Ace
+                     //User chooses value to equal 1 or 11
+                     //User Validation
+             //If Card 4 is an Ace
+                     //User chooses value to equal 1 or 11
+                     //User Validation
+             //If Card 5 is an Ace
+                     //User chooses value to equal 1 or 11
+                     //User Validation
+             //If Card 6 is an Ace
+                     //User chooses value to equal 1 or 11
+                     //User Validation
+             
+        //Math for sum of the 6 cards
+             //Shows Card 1 Value
+             //Shows Card 2 Value
+             //Shows card 3 value
+             //Show card 4 Value
+             //Show card 5 Value   
+             //Show card 6 value
+             //Dssplays Sum 
+             //Shows Dealers Card
+             //ask to hit or stay again=false
+             //Goes to Dealers turn=true
+             //If sum Greater than 21, user busts. So
+                                    //skip Dealers turn.
+    
+    
+    
+    //When the player Holds
+    //Dealers 2nd card
+    //if goDeal=true
+       //Type Y for dealer to show 2nd card
+        //User Validation
+}       //when (Y), gets dealers 2nd card
+        //Dealers Card 2 is assigned a face and number value 
+        //Displays dealers 1st card
+        //Displays dealers 2nd card
+        //if Dealer's 1st Card is an Ace
+              //Dealer chooses ace to = 11
+             //Dealer choooses ace to =1
+        //if Dealer's 2nd Card is an Ace
+            //Chooses Ace=11
+            //Dealer chooses Ace=1
+      //Math for sum of the dealers 2 cards
+      //Displays Dealer's Sum
+      //Displays User's Sum
+        
+        
+    
+        //if Dealers sum is <17 he hits
+        //the dealer hits
+              //Type Y to continue
+              //User Validation
+}                //when (Y), gets dealers 3rd card
+                  //Dealers Card 3 is assigned a face and number value 
+                  //Displays dealers 1st card
+                  //Displays dealers 2nd card
+                  //Dislpay dealers 3rd card
+         //if Dealer's 1st Card is an Ace
+              //Dealer chooses ace to = 11
+             //Dealer choooses ace to =1
+        //if Dealer's 2nd Card is an Ace
+             //Chooses Ace=11
+             //Dealer chooses Ace=1
+         //if Dealer's 3rd Card is an Ace
+             //Chooses Ace=11
+             //Dealer chooses Ace=1
+      //Math for sum of the dealers 3 cards
+      //Displays Dealer's Sum
+      //Displays User's Sum
+        
+    
+ //if Dealers sum is <17 he hits
+        //the dealer hits
+              //Type Y to continue
+              //User Validation
+}                //when (Y), gets dealers 4th card
+                  //Dealers Card 4 is assigned a face and number value 
+                  //Displays dealers 1st card
+                  //Displays dealers 2nd card
+                  //Dislpay dealers 3rd card
+                  //Display Dealers 4th card
+         //if Dealer's 1st Card is an Ace
+              //Dealer chooses ace to = 11
+             //Dealer choooses ace to =1
+        //if Dealer's 2nd Card is an Ace
+             //Chooses Ace=11
+             //Dealer chooses Ace=1
+         //if Dealer's 3rd Card is an Ace
+             //Chooses Ace=11
+             //Dealer chooses Ace=1
+         //if Dealer's 4th Card is an Ace
+             //Chooses Ace=11
+             //Dealer chooses Ace=1
+      //Math for sum of the dealers 4 cards
+      //Displays Dealer's Sum
+      //Displays User's Sum
+        
+    
+//if Dealers sum is <17 he hits
+        //the dealer hits
+              //Type Y to continue
+              //User Validation
+}                //when (Y), gets dealers 5th card
+                  //Dealers Card 5 is assigned a face and number value 
+                  //Displays dealers 1st card
+                  //Displays dealers 2nd card
+                  //Dislpay dealers 3rd card
+                  //Display Dealers 4th card
+                  //Display dealers 5th card
+         //if Dealer's 1st Card is an Ace
+              //Dealer chooses ace to = 11
+             //Dealer choooses ace to =1
+        //if Dealer's 2nd Card is an Ace
+             //Chooses Ace=11
+             //Dealer chooses Ace=1
+         //if Dealer's 3rd Card is an Ace
+             //Chooses Ace=11
+             //Dealer chooses Ace=1
+         //if Dealer's 4th Card is an Ace
+             //Chooses Ace=11
+             //Dealer chooses Ace=1
+          //if Dealer's 5th Card is an Ace
+             //Chooses Ace=11
+             //Dealer chooses Ace=1
+      //Math for sum of the dealers 5 cards
+      //Displays Dealer's Sum
+      //Displays User's Sum
+
+      
+//if Dealers sum is <17 he hits
+        //the dealer hits
+              //Type Y to continue
+              //User Validation
+}                //when (Y), gets dealers 5th card
+                  //Dealers Card 6 is assigned a face and number value 
+                  //Displays dealers 1st card
+                  //Displays dealers 2nd card
+                  //Dislpay dealers 3rd card
+                  //Display Dealers 4th card
+                  //Display dealers 5th card
+                  //Display Dealers 6th card
+         //if Dealer's 1st Card is an Ace
+              //Dealer chooses ace to = 11
+             //Dealer choooses ace to =1
+        //if Dealer's 2nd Card is an Ace
+             //Chooses Ace=11
+             //Dealer chooses Ace=1
+         //if Dealer's 3rd Card is an Ace
+             //Chooses Ace=11
+             //Dealer chooses Ace=1
+         //if Dealer's 4th Card is an Ace
+             //Chooses Ace=11
+             //Dealer chooses Ace=1
+          //if Dealer's 5th Card is an Ace
+             //Chooses Ace=11
+             //Dealer chooses Ace=1
+          //if Dealer's 6th Card is an Ace
+             //Chooses Ace=11
+             //Dealer chooses Ace=1
+      //Math for sum of the dealers 6 cards
+      //Displays Dealer's Sum
+      //Displays User's Sum
+    
+    //zesults function
+                               |
+//Asks User if they want to play again
+   //Validate User Input
+    //If (Y) game restarts
+   //if (N), game ends
+   
+   //exit stage right!
---
+++ Projects/Final_PseudoCode/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=final_pseudocode
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/final_pseudocode
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=finalpseudocode.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/finalpseudocode.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=final_pseudocode
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/final_pseudocode
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=finalpseudocode.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/finalpseudocode.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
